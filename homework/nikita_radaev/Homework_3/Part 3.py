4a854aa2e30bdbeed9a532b0b39879858637b846
NikitaRadaev <radaevnick@yandex.ru>
2025-03-31 00:14:22 +0300

    homework_3_done

--- homework/nikita_radaev/Homework_3/Part 3.py
+++ homework/nikita_radaev/Homework_3/Part 3.py
@@ -1,4 +1,4 @@
 x = 15
 y = 10
-print ((x-y) / (1 + (x * y)))
-print ((x + y) / 2)
+print((x - y) / (1 + (x * y)))
+print((x + y) / 2)
--- homework/nikita_radaev/Homework_3/Part 4.py
+++ homework/nikita_radaev/Homework_3/Part 4.py
@@ -1,5 +1,4 @@
 x = 6
 y = 7
-print ((x ** 0.5) + (y ** 0.5))
-print (x * y)
-
+print((x ** 0.5) + (y ** 0.5))
+print(x * y)
