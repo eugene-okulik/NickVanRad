85e197eb5886208e03eb845b757f5c39ad60414f
NikitaRadaev <radaevnick@yandex.ru>
2025-04-04 14:56:30 +0300

    Homework5_is_done

--- homework/nikita_radaev/Homework_5/HomeWork5.py
+++ homework/nikita_radaev/Homework_5/HomeWork5.py
@@ -4,17 +4,22 @@ print(a, b, c, d, e)
 
 
 text_1 = 'результат операции: 42'
-a = text_1[-2:]
-print(int(a) + 10)
-text_2 = 'результат операции: 514'
-b = text_2[-3:]
+a = text_1.index(': ') + 1
+b = text_1[a:]
 print(int(b) + 10)
+text_2 = 'результат операции: 514'
+c = text_2.index(': ') + 1
+d = text_2[c:]
+print(int(d) + 10)
 text_3 = 'результат работы программы: 9'
-c = text_3[-1:]
-print(int(c) + 10)
+e = text_3.index(': ') + 1
+f = text_3[e:]
+print(int(f) + 10)
 
 students = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
 students = ', '.join(students)
 subjects = ', '.join(subjects)
 print(students, 'study these subjects:', subjects)
+
+
