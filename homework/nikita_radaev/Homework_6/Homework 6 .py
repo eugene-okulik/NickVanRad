d4af4eff1bd8c652ab8a070be20dd3c30ddeefbc
NikitaRadaev <radaevnick@yandex.ru>
2025-04-10 00:03:41 +0300

    Homework_6_is_done

---
+++ homework/nikita_radaev/Homework_6/Homework 6 .py
@@ -0,0 +1,9 @@
+text = "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
+list_homework = text.split()
+for texts in list_homework :
+    if texts[-1] in '.,':
+        punct = texts[-1]
+        print(texts[:-1] + 'ing' + punct, end=' ')
+    else :
+        print(texts + 'ing', end=' ')
+
