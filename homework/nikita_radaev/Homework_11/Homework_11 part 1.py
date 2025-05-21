21a6febfc615dcff5ab858242f7a6f19557d2964
NikitaRadaev <radaevnick@yandex.ru>
2025-05-21 17:23:22 +0300

    Homework_is_Done

--- homework/nikita_radaev/Homework_11/Homework_11 part 1.py
+++ homework/nikita_radaev/Homework_11/Homework_11 part 1.py
@@ -1,15 +1,16 @@
 class Book:
     material = "бумага"
     text = True
+    reserved = False
 
-    def __init__(self, title, avtor, numpage, ISBN, reserved):
+    def __init__(self, title, avtor, numpage, ISBN):
         self.title = title
         self.avtor = avtor
         self.numpage = numpage
         self.ISBN = ISBN
-        self.reserved = reserved
 
     def get_data(self):
+
         if self.reserved is True:
             print("Название :", self.title, "Автор :", self.avtor, "cтраниц :", self.numpage, "материал :",
                   Book.material,
@@ -19,10 +20,35 @@ class Book:
                   Book.material)
 
 
-books = Book("Идиот,", "Достоевский,", "500,", 2424, reserved=False)
-books_1 = Book("Му-Му,", "Тургенев,", "550,", 2424, reserved=True)
-books_2 = Book("Властелин колец,", "Толкиен,", "1500,", 2424, reserved=False)
-books_3 = Book("Мертвые души,", "Гоголь,", "400,", 2424, reserved=True)
-books_4 = Book("Шарик в гостях у Барбоса,", "Носов,", "500,", 2424, reserved=False)
+class SchoolBook(Book):
+    def __init__(self, title, avtor, numpage, ISBN, subject, klass, task):
+        super().__init__(title, avtor, numpage, ISBN)
+        self.subject = subject
+        self.klass = klass
+        self.task = task
 
-books_1.get_data()
+    def get_data1(self):
+
+        if self.reserved is True:
+            print("Название :", self.title, "Автор :", self.avtor, "cтраниц :", self.numpage, "предмет :",
+                  self.subject, "класс :", self.klass, ", зарезервирована")
+        else:
+            print("Название :", self.title, "Автор :", self.avtor, "cтраниц :", self.numpage, "предмет :",
+                  self.subject, "класс :", self.klass)
+
+
+books_5 = SchoolBook("Алгебра", "Толстой", "500", "34534", "Математика", "7А",
+                     "True")
+books_6 = SchoolBook("Геометрия", "Ленин", "600", "54534", "Математика", "7Б",
+                     "True")
+books_5.reserved = True
+books_5.get_data1()
+
+books = Book("Идиот,", "Достоевский,", "500,", 2424)
+books_1 = Book("Му-Му,", "Тургенев,", "550,", 2424)
+books_2 = Book("Властелин колец,", "Толкиен,", "1500,", 2424)
+books_3 = Book("Мертвые души,", "Гоголь,", "400,", 2424)
+books_4 = Book("Шарик в гостях у Барбоса,", "Носов,", "500,", 2424)
+
+books_2.reserved = True
+books_2.get_data()
